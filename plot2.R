8fa33b045b22f4122ef1e5e677e668ed50d3f45f
Chris Shumaker <chris.shumaker@gmail.com>
2016-05-04 22:06:47 -0400

    Completed plot 2

---
+++ plot2.R
@@ -0,0 +1,11 @@
+hhpc <- read.delim(file="household_power_consumption.txt", sep=";", nrows = 2880, skip = 66636, header = TRUE)
+headers <- read.delim(file="household_power_consumption.txt", sep=";", nrows = 1, header = TRUE)
+colnames(hhpc) <- colnames(headers)
+
+hhpc$Date <- as.Date(strptime(hhpc$Date, "%e/%m/%Y"))
+
+hhpc$Days <- as.factor(weekdays(hhpc$Date))
+
+plot(hhpc$Global_active_power, type="l", xlab='', ylab ="Global Active Power (kilowatts)", xaxt='n')
+
+axis(side = 1, at=c(0, 1440, 2880), labels= c("Thu", "Fri", "Sat"), tick = TRUE)
